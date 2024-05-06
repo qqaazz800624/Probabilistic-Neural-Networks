efd4f4ea4a85ed121d4465c729040ad83ef23b33
qqaazz800624 <qqaazz800624@gmail.com>
2024-05-06 16:33:19 +0800

    add data splitting script of montgomery

---
+++ scripts/split_datalist_montgomery.py
@@ -0,0 +1,67 @@
+#%%
+
+
+import os
+import json
+from glob import glob
+from sklearn.model_selection import KFold
+
+data_root = '/home/u/qqaazz800624/Probabilistic-Neural-Networks/data/MontgomerySet'
+image_dir = os.path.join(data_root, 'CXR_png')
+label_dir = os.path.join(data_root, 'ManualMask')
+combined_masks_dir = os.path.join(label_dir, 'combinedMask')
+
+
+def main():
+    image_dir = os.path.join(data_root, 'CXR_png')
+    mask_dir = os.path.join(data_root, 'ManualMask', 'combinedMask')
+    # Get list of all image files
+    image_files = glob(os.path.join(image_dir, '*.png'))
+
+    # Prepare the data list
+    data_list = []
+    
+    for image_path in image_files:
+        filename = os.path.basename(image_path)
+        mask_path = os.path.join(mask_dir, filename)
+        
+        # Check if corresponding mask exists
+        if os.path.exists(mask_path):
+            data_list.append({
+                'filename': filename,
+                'image': image_path,
+                'taget': mask_path
+            })
+
+    five_fold_datalist = {}
+    for k, (_, fold) in enumerate(KFold(n_splits=5, shuffle=True, random_state=42).split(data_list)):
+        five_fold_datalist[f"fold_{k}"] = [data_list[i] for i in fold]
+    
+    # Write to JSON file
+    # with open(os.path.join(data_root, 'datalist_montgomery.json'), 'w') as outfile:
+    #     json.dump(data_list, outfile, indent=4)
+
+    with open(os.path.join(data_root, 'datalist_fold_montgomery.json'), 'w') as outfile:
+        json.dump(five_fold_datalist, outfile, indent=4)
+
+# Call the function
+#create_data_list(image_dir, combined_masks_dir)
+
+if __name__ == '__main__':
+    main()
+
+#%%
+
+
+
+
+
+
+#%%
+
+
+
+
+
+
+#%%
