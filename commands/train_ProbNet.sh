d5ba486e363363b4ce293a3712e7bb8c5d8fe971
qqaazz800624 <qqaazz800624@gmail.com>
2024-05-20 15:22:09 +0800

    add default setting to control GPU devices

--- commands/train_ProbNet.sh
+++ commands/train_ProbNet.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Set default path for the Python script
-DEFAULT_PYTHON_SCRIPT="./scripts/prob_u_net_montgomery.py"
+DEFAULT_PYTHON_SCRIPT="./scripts/prob_u_net_siim.py"
 
 # Allow overriding the default path with a command-line argument
-PYTHON_SCRIPT=${1:-$DEFAULT_PYTHON_SCRIPT}
+PYTHON_SCRIPT=${2:-$DEFAULT_PYTHON_SCRIPT}
+
+# Set default CUDA device to use
+DEFAULT_CUDA_DEVICE="1"
+
+# Allow overriding the default CUDA device with a command-line argument
+CUDA_DEVICE=${1:-$DEFAULT_CUDA_DEVICE}
 
 # Set the CUDA device to use
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=$CUDA_DEVICE
 
 # Run the Python script with the specified or default path
 python "$PYTHON_SCRIPT"
