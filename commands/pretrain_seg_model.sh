1289f63b3fa675e6b23b7b31981e7ce071033dcd
qqaazz800624 <qqaazz800624@gmail.com>
2024-05-27 17:00:47 +0800

    update shell script

--- commands/pretrain_seg_model.sh
+++ commands/pretrain_seg_model.sh
@@ -4,13 +4,13 @@
 DEFAULT_PYTHON_SCRIPT="./scripts/pretraining_siim.py"
 
 # Allow overriding the default path with a command-line argument
-PYTHON_SCRIPT=${1:-$DEFAULT_PYTHON_SCRIPT}
+PYTHON_SCRIPT=${2:-$DEFAULT_PYTHON_SCRIPT}
 
 # Set default CUDA device to use
 DEFAULT_CUDA_DEVICE="1"
 
 # Allow overriding the default CUDA device with a command-line argument
-CUDA_DEVICE=${2:-$DEFAULT_CUDA_DEVICE}
+CUDA_DEVICE=${1:-$DEFAULT_CUDA_DEVICE}
 
 # Set the CUDA device to use
 export CUDA_VISIBLE_DEVICES=$CUDA_DEVICE
