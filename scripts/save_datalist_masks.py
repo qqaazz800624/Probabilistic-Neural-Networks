d03d7129dc183e1aa0303c04c2088e109c645079
qqaazz800624 <qqaazz800624@gmail.com>
2024-06-26 07:20:31 +0000

    datalist with uncertainty masks paths

--- scripts/save_datalist_masks.py
+++ scripts/save_datalist_masks.py
@@ -16,13 +16,27 @@ for fold in folds:
     for item in data_list[fold]:
         image_path = item['image']
         base_name = os.path.basename(image_path)
-        uncertaunty_mask_path = f"data/Masks/{base_name}"
+        uncertaunty_mask_path = f"masks/{base_name}"
         item['mask'] = uncertaunty_mask_path
 
 #%%
 
 with open(os.path.join(personal_root, 'datalist_masks.json'), 'w') as f:
     json.dump(data_list, f)
+
+
+#%%
+
+import os, json
+
+personal_root: str = '/home/u/qqaazz800624/Probabilistic-Neural-Networks/data'
+json_file: str = 'datalist_masks.json'
+json_path = os.path.join(personal_root, json_file)
+
+with open(json_path) as f:
+    data_list = json.load(f)
+
+data_list
 
 
 #%%
